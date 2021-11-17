38af1155fbee51aa2a3ee7e2a6911515c17ff1ac
aungpkhant <derrick.aungkhant@gmail.com>
2021-11-17 21:25:18 +0630

    ✨ Project setup

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  env: {
+    node: true,
+    es6: true,
+  },
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  ignorePatterns: ['node_modules/*'],
+};
